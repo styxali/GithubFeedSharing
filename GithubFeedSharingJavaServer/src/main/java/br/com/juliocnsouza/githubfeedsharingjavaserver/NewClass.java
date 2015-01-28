52d5aeccd3546088bbd6723cbe6edf98e8017903
juliocnsouza <juliocnsouzadev@gmail.com>
2015-01-28 01:25:03 -0200

    update

---
+++ GithubFeedSharingJavaServer/src/main/java/br/com/juliocnsouza/githubfeedsharingjavaserver/NewClass.java
@@ -0,0 +1,13 @@
+package br.com.juliocnsouza.githubfeedsharingjavaserver;
+
+
+/**
+ * NewClass.java -> Job:
+ * @since 28/01/2015
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+
+public class NewClass {
+
+}
